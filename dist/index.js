209916fccacbd33cb8cda6e6ae2e7de055c6c82e
Jun-cloud <kjy8827@gmail.com>
2020-07-26 15:00:19 +0900

    Create function - create new block

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// yarn add crypto-js
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,7 +11,20 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+// CryptoJS.SHA256()를 사용하여 암호화
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 const genesisBlock = new Block(0, "56918234", "", "block", 12345);
 let blockChain = [genesisBlock];
-console.log(blockChain);
+const getBlockChain = () => blockChain;
+const getLatestBlock = () => blockChain[blockChain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+console.log(createNewBlock("New"), createNewBlock("Block"));
 //# sourceMappingURL=index.js.map
